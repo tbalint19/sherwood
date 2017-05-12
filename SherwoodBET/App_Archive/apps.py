2ab7afa6cf68add4b634f7ee4e4654dd10cd0f1f
tbalint19 <toth910719balint@gmail.com>
2017-05-12 19:11:47 +0200

    apps created

---
+++ SherwoodBET/App_Archive/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppArchiveConfig(AppConfig):
+    name = 'App_Archive'
---
+++ SherwoodBET/App_Wall/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppWallConfig(AppConfig):
+    name = 'App_Wall'
