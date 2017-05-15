2d4bae23fa531e3d94063313322bb528ed8ad64c
tbalint19 <toth910719balint@gmail.com>
2017-05-15 18:55:49 +0200

    signup tests refactored

--- SherwoodBET/App_Profile/tests.py
+++ SherwoodBET/App_Profile/tests.py
@@ -7,60 +7,58 @@ import json
 
 class SignupDataFactory:
 
-    def create_json(self, username="NewUser1", email="new@user1.com", password="12345Ab", missing=None):
+    def json(self, username="NewUser1", email="new@user1.com", password="12345Ab", missing=None):
         data = {'username': username, 'email': email, 'password': password}
         if missing is not None:
             del data[missing]
         return json.dumps(data)
 
+class SignupTestUser:
 
-class User_test(TestCase):
+    def __init__(self, client):
+        self.client = client
+
+    def post(self, data):
+        return self.client.post(reverse('signup_user'), data, content_type='application/json')
+
+
+class Signup_test(TestCase):
 
     def setUp(self):
-        self.factory = SignupDataFactory()
         self.user_in_db = User.objects.create_user(
             username='DefaultInDB', email='Default@indb.hu', password='123456Ab')
         Profile(user_obj=self.user_in_db).save()
         Account(user_obj=self.user_in_db).save()
+        self.user = SignupTestUser(self.client)
+        self.factory = SignupDataFactory()
 
     def test_signup_sunnyday(self):
-        request_data = self.factory.create_json()
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        user_exists = User.objects.filter(username="NewUser1").exists()
-        self.assertTrue(user_exists)
+        response = self.user.post(self.factory.json())
+        self.assertTrue(User.objects.filter(username="NewUser1").exists())
         self.assertEqual(json.loads(response.content.decode('utf-8'))['content'], {'errors': []})
 
     def test_signup_same_username(self):
-        request_data = self.factory.create_json(username="DefaultInDB")
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        users = User.objects.filter(username="DefaultInDB")
-        self.assertEqual(len(users), 1)
+        response = self.user.post(self.factory.json(username="DefaultInDB"))
+        self.assertEqual(len(User.objects.filter(username="DefaultInDB")), 1)
         self.assertEqual(json.loads(response.content.decode('utf-8'))['content'], {'errors': ["username"]})
 
     def test_signup_same_email(self):
-        request_data = self.factory.create_json(email="Default@indb.hu")
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        users = User.objects.filter(email="Default@indb.hu")
-        self.assertEqual(len(users), 1)
+        response = self.user.post(self.factory.json(email="Default@indb.hu"))
+        self.assertEqual(len(User.objects.filter(email="Default@indb.hu")), 1)
         self.assertEqual(json.loads(response.content.decode('utf-8'))['content'], {'errors': ["email"]})
 
     def test_signup_same_credentials(self):
-        request_data = self.factory.create_json(username="DefaultInDB", email="Default@indb.hu")
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
+        response = self.user.post(self.factory.json(username="DefaultInDB", email="Default@indb.hu"))
         errors = json.loads(response.content.decode('utf-8'))['content']['errors']
         self.assertTrue("username" in errors and "email" in errors)
 
     def test_user_extensions_created(self):
-        request_data = self.factory.create_json()
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        profile_exists = Profile.objects.filter(user_obj__username="NewUser1").exists()
-        account_exists = Account.objects.filter(user_obj__username="NewUser1").exists()
-        self.assertTrue(profile_exists)
-        self.assertTrue(account_exists)
+        response = self.user.post(self.factory.json())
+        self.assertTrue(Profile.objects.filter(user_obj__username="NewUser1").exists())
+        self.assertTrue(Account.objects.filter(user_obj__username="NewUser1").exists())
 
     def test_user_extension_with_proper_data(self):
-        request_data = self.factory.create_json()
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
+        response = self.user.post(self.factory.json())
         profile = Profile.objects.get(user_obj__username="NewUser1")
         account = Account.objects.get(user_obj__username="NewUser1")
         self.assertEqual(profile.rank, "newbie")
@@ -72,92 +70,67 @@ class User_test(TestCase):
         self.assertEqual(account.real_money, 0)
 
     def test_no_additional_extensions_created(self):
-        request_data = self.factory.create_json(username="DefaultInDB")
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        profiles = Profile.objects.filter(user_obj__username="DefaultInDB")
-        accounts = Account.objects.filter(user_obj__username="DefaultInDB")
-        self.assertEqual(len(profiles), 1)
-        self.assertEqual(len(accounts), 1)
+        response = self.client.post(self.factory.json(username="DefaultInDB"))
+        self.assertEqual(len(Profile.objects.filter(user_obj__username="DefaultInDB")), 1)
+        self.assertEqual(len(Account.objects.filter(user_obj__username="DefaultInDB")), 1)
 
     def test_invalid_signup_data_username_missing(self):
-        request_data = self.factory.create_json(missing='username')
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        user_exists = User.objects.filter(email="new@user1.hu").exists()
-        self.assertFalse(user_exists)
+        response = self.user.post(self.factory.json(missing='username'))
+        self.assertFalse(User.objects.filter(email="new@user1.hu").exists())
         self.assertEqual(json.loads(response.content.decode('utf-8'))['content'], {'errors': []})
 
     def test_invalid_signup_data_email_missing(self):
-        request_data = self.factory.create_json(missing='email')
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        user_exists = User.objects.filter(username="NewUser1").exists()
-        self.assertFalse(user_exists)
+        response = self.user.post(self.factory.json(missing='email'))
+        self.assertFalse(User.objects.filter(username="NewUser1").exists())
         self.assertEqual(json.loads(response.content.decode('utf-8'))['content'], {'errors': []})
 
     def test_invalid_signup_data_password_missing(self):
-        request_data = self.factory.create_json(missing='password')
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        user_exists = User.objects.filter(email="NewUser1").exists()
-        self.assertFalse(user_exists)
+        response = self.user.post(self.factory.json(missing='password'))
+        self.assertFalse(User.objects.filter(email="NewUser1").exists())
         self.assertEqual(json.loads(response.content.decode('utf-8'))['content'], {'errors': []})
 
     def test_signup_with_short_username(self):
-        request_data = self.factory.create_json(username='only6c')
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        user_exists = User.objects.filter(username="only6c").exists()
-        self.assertFalse(user_exists)
+        response = self.user.post(self.factory.json(username='only6c'))
+        self.assertFalse(User.objects.filter(username="only6c").exists())
         self.assertEqual(json.loads(response.content.decode('utf-8'))['content'], {'errors': []})
 
     def test_signup_with_short_email(self):
-        request_data = self.factory.create_json(email='A1@2.3')
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        user_exists = User.objects.filter(email="1@2.3").exists()
-        self.assertFalse(user_exists)
+        response = self.user.post(self.factory.json(email='A1@2.3'))
+        self.assertFalse(User.objects.filter(email="1@2.3").exists())
         self.assertEqual(json.loads(response.content.decode('utf-8'))['content'], {'errors': []})
 
     def test_signup_with_short_password(self):
-        request_data = self.factory.create_json(password='only6c')
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        user_exists = User.objects.filter(username="KovacsBela").exists()
-        self.assertFalse(user_exists)
+        response = self.user.post(self.factory.json(password='only6c'))
+        self.assertFalse(User.objects.filter(username="KovacsBela").exists())
         self.assertEqual(json.loads(response.content.decode('utf-8'))['content'], {'errors': []})
 
     def test_signup_with_long_username(self):
-        request_data = self.factory.create_json(username=''.join(["B" for x in range(51)]))
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        user_exists = User.objects.filter(email="new@user1.hu").exists()
-        self.assertFalse(user_exists)
+        response = self.user.post(self.factory.json(username=''.join(["B" for x in range(51)])))
+        self.assertFalse(User.objects.filter(email="new@user1.hu").exists())
         self.assertEqual(json.loads(response.content.decode('utf-8'))['content'], {'errors': []})
 
     def test_signup_with_long_email(self):
-        request_data = self.factory.create_json(username=''.join(["B" for x in range(46)]) + "@x.hu")
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        user_exists = User.objects.filter(username="NewUser1").exists()
-        self.assertFalse(user_exists)
+        response = self.user.post(self.factory.json(username=''.join(["B" for x in range(46)]) + "@x.hu"))
+        self.assertFalse(User.objects.filter(username="NewUser1").exists())
         self.assertEqual(json.loads(response.content.decode('utf-8'))['content'], {'errors': []})
 
     def test_signup_with_long_password(self):
-        request_data = self.factory.create_json(password=''.join(["B" for x in range(51)]))
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        user_exists = User.objects.filter(username="KovacsBela").exists()
-        self.assertFalse(user_exists)
+        response = self.user.post(self.factory.json(password=''.join(["B" for x in range(51)])))
+        self.assertFalse(User.objects.filter(username="KovacsBela").exists())
         self.assertEqual(json.loads(response.content.decode('utf-8'))['content'], {'errors': []})
 
     def test_signup_with_invalid_email(self):
-        request_data = self.factory.create_json(email="onlychar")
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        user_exists = User.objects.filter(email="onlychar").exists()
-        self.assertFalse(user_exists)
+        response = self.user.post(self.factory.json(email="onlychar"))
+        errors = json.loads(response.content.decode('utf-8'))['content']['errors']
+        self.assertFalse(User.objects.filter(email="onlychar").exists())
+        self.assertEqual(errors, [])
 
     def test_signup_with_upper_domain_email(self):
-        request_data = self.factory.create_json(email="with@upper.com")
-        self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
-        request_data = self.factory.create_json(email="with@UPPER.com", username="NewUser2")
-        response = self.client.post(reverse('signup_user'), data=request_data, content_type='application/json')
+        self.user.post(self.factory.json(email="with@upper.com"))
+        response = self.user.post(self.factory.json(email="with@UPPER.com", username="NewUser2"))
         errors = json.loads(response.content.decode('utf-8'))['content']['errors']
-        user_exists_with_upper = User.objects.filter(email="with@UPPER.com").exists()
-        user_exists_with_lower = User.objects.filter(email="with@upper.com").exists()
-        self.assertFalse(user_exists_with_upper)
-        self.assertTrue(user_exists_with_lower)
+        self.assertFalse(User.objects.filter(email="with@UPPER.com").exists())
+        self.assertTrue(User.objects.filter(email="with@upper.com").exists())
         self.assertEqual(len(User.objects.filter(username="NewUser1")), 1)
         self.assertEqual(len(User.objects.filter(username="NewUser2")), 0)
         self.assertEqual(len(User.objects.filter(email="with@upper.com")), 1)
