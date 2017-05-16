73ce9a9e36be537ab3fdbcc5b03d7599f28e56de
tbalint19 <toth910719balint@gmail.com>
2017-05-16 12:53:16 +0200

    first tests login

--- SherwoodBET/App_Profile/tests.py
+++ SherwoodBET/App_Profile/tests.py
@@ -134,3 +134,44 @@ class Signup_test(TestCase):
         self.assertEqual(len(User.objects.filter(username="NewUser1")), 1)
         self.assertEqual(len(User.objects.filter(username="NewUser2")), 0)
         self.assertEqual(len(User.objects.filter(email="with@upper.com")), 1)
+
+class LoginDataFactory:
+
+    def json(self, username="DefaultInDB", email="Default@indb.hu", password="12345Ab", missing=None, credential="username"):
+        data = {'password': password}
+        data[credential] = vars()[credential]
+        if missing is not None:
+            del data[missing]
+        return json.dumps(data)
+
+class LoginTestUser:
+
+    def __init__(self, client):
+        self.client = client
+
+    def post(self, data):
+        return self.client.post(reverse('login_user'), data, content_type='application/json')
+
+class Login_test(TestCase):
+
+    def setUp(self):
+        self.user = LoginTestUser(self.client)
+        self.factory = LoginDataFactory()
+        self.user_in_db = User.objects.create_user(
+            username='DefaultInDB', email='Default@indb.hu', password='12345Ab')
+
+    def test_sunny_day_login(self):
+        data = self.factory.json()
+        response = self.user.post(data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content.decode('utf-8'))['content'], {'is_successful': True})
+
+    def test_wrong_password(self):
+        data = self.factory.json(password="54321Ab")
+        response = self.user.post(data)
+        self.assertEqual(json.loads(response.content.decode('utf-8'))['content'], {'is_successful': False})
+
+    def test_sunny_day_login_email(self):
+        data = self.factory.json(credential="email")
+        response = self.user.post(data)
+        self.assertEqual(json.loads(response.content.decode('utf-8'))['content'], {'is_successful': True})
--- SherwoodBET/App_Profile/views.py
+++ SherwoodBET/App_Profile/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
 from App_Profile.models import Profile, Account
 from App_Profile.validators import SignupRequestValidator
 from _Middleware import API
+import json
 
 @API.public
 def signup_user(request):
@@ -16,7 +18,16 @@ def signup_user(request):
 
 @API.public
 def login_user(request):
-    pass
+    user_data = json.loads(request.body.decode('utf-8'))
+    password = user_data["password"]
+    if "username" in user_data:
+        user = authenticate(request, username=user_data["username"], password=password)
+    if "email" in user_data:
+        user = authenticate(request, username=User.objects.get(email=user_data["email"]).username, password=password)
+    is_authenticated = user is not None
+    if is_authenticated:
+        login(request, user)
+    return {'is_successful': is_authenticated}
 
 @API.user
 def logout_user(request):
