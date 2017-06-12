1c1386c562dfa6223fd4627f087214288e04007d
tbalint19 <toth910719balint@gmail.com>
2017-06-12 08:38:35 +0200

    before basic frontend

--- SherwoodBET/App_Wall/models.py
+++ SherwoodBET/App_Wall/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
-class Story(models.Model):
-    ANNOUNCEMENT = "Announcement"
-    NEWS = "News"
-    MATCH = "Match"
-    PREVIEW = "Preview"
-    content_type_choices = ((ANNOUNCEMENT, "Announcement"), (NEWS, "News"), (MATCH, "Match"), (PREVIEW, "Preview"))
-
-    content_type = models.CharField(max_length=12, choices=content_type_choices, default=ANNOUNCEMENT)
-    text = models.CharField(max_length=150)
-    link = models.CharField(max_length=150)
-    created = models.DateTimeField()
-
-class Advert(models.Model):
-
-    text = models.CharField(max_length=150)
+# class Story(models.Model):
+#     ANNOUNCEMENT = "Announcement"
+#     NEWS = "News"
+#     MATCH = "Match"
+#     PREVIEW = "Preview"
+#     content_type_choices = ((ANNOUNCEMENT, "Announcement"), (NEWS, "News"), (MATCH, "Match"), (PREVIEW, "Preview"))
+#
+#     content_type = models.CharField(max_length=12, choices=content_type_choices, default=ANNOUNCEMENT)
+#     text = models.CharField(max_length=150)
+#     link = models.CharField(max_length=150)
+#     created = models.DateTimeField()
+#
+# class Advert(models.Model):
+#
+#     text = models.CharField(max_length=150)
